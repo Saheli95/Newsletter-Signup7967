b9edf0d5c7542d864775ecc13dad0326db5e03a5
Saheli <sahelibehera2016@gmail.com>
2020-11-20 18:22:10 +0530

    Changed port to PORT

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post("/failure", function(req,res){
   res.redirect("/");
 })
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
 
